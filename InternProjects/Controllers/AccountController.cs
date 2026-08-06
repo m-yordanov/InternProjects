0d46623376df33ef323cee1c6a88ff1909f8b842
m-yordanov <114444124+m-yordanov@users.noreply.github.com>
2026-08-06 13:06:03 +0300

    Fix account controller typos

--- InternProjects/Controllers/AccountController.cs
+++ InternProjects/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
-namespace ТвоятProject.Controllers
+namespace InternProjects.Controllers
 {
     public class AccountController : Controller
     {
